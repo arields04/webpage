c2f5c5496992ad68f815bbb8573d3028e37b4c33
arields04 <arields04@hotmail.com>
2026-01-14 20:35:34 -0300

    Sincronización automática desde VS Code

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+    const icon = toggleButton.querySelector('i');
+
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+
+        // Cambiar icono entre Luna y Sol
+        if (body.classList.contains('dark-mode')) {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+        } else {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+        }
+    });
+});
